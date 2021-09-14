8e00f247098bc686ceb18cc3c16d2ecec405e76c
Morgan Hofmann <v6in2-morgan@6l-ltinf07.atlas.atlascollege.nl>
2021-09-14 09:48:22 +0200

    Initial commit

---
+++ js/engine.js
@@ -0,0 +1,95 @@
+class Scene {
+  loaded = false;
+
+  constructor() {
+
+  }
+
+  update(ctx) {
+    if (!this.loaded) return;
+
+    // Run upon request for an animation frame, or once per frame.
+
+    // This method should be overridden in a scene class which extends this.
+  }
+
+  load() {
+    this.loaded = true;
+    return this;
+  }
+
+  unload() {
+    this.loaded = false;
+    return this;
+  }
+}
+
+class SceneManager {
+  loadedScenes = [];
+  canvas = null;
+  ctx = null;
+
+  constructor() {
+
+  }
+
+  init() {
+    this.canvas = window.engine.canvas;
+    this.ctx = window.engine.ctx;
+    window.requestAnimationFrame(this.update);
+
+    console.log("Scene Manager initialized.");
+  }
+
+  update = (step) => {
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.loadedScenes.forEach((scene) => {
+      scene.update(this.ctx, step);
+    });
+    window.requestAnimationFrame(this.update);
+  };
+
+  loadSingle(scene) {
+    this.loadedScenes.forEach((scene) => {
+      scene.unload();
+    });
+
+    var s = this.load(scene);
+
+    this.loadedScenes = [s];
+  }
+
+  loadAdd(scene) {
+    var s = this.load(scene);
+
+    this.loadedScenes.add(s);
+  }
+
+  load(scene) {
+    if (!(scene instanceof Scene)) {
+      return console.error("Tried to load something that isn't a scene.");
+    }
+
+    return scene.load();
+  }
+}
+
+class Engine {
+  canvas = null; // Canvas element that is bound to the engine.
+  ctx = null; // Canvas context for short-hand access.
+
+  constructor(canvas) {
+    console.log("Starting engine...");
+
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+
+    this.sceneManager = new SceneManager();
+  }
+
+  init() {
+    this.sceneManager.init();
+  }
+}
---
+++ js/game.js
@@ -0,0 +1,30 @@
+class MainMenu extends Scene {
+  update(ctx, step) {
+    ctx.fillStyle = `rgb(${ (step / 10) % 255 },0,0)`;
+    ctx.fillRect(10, 10, 100, 100);
+
+    ctx.fillStyle = `rgb(0,0,${ (step / 10) % 255 })`;
+    ctx.fillRect(120, 10, 100, 100);
+  }
+}
+
+// Bind the game engine to a canvas element.
+function bind(canvas) {
+  if (!(canvas instanceof HTMLCanvasElement)) {
+    return console.error("Could not bind to canvas: given object was not a canvas.");
+  }
+
+  window.engine = new Engine(canvas);
+  window.engine.init();
+  console.log("Engine bound to", canvas);
+
+  init();
+}
+
+function init() {
+  window.engine.sceneManager.loadSingle(new MainMenu());
+}
+
+window.onload = () => {
+  bind(document.getElementById('canvas'));
+};
